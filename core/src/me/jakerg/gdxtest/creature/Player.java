94d21e97df0e605ae6aaac149a793db25b32f103
Jake Gutierrez <jakegut0108@gmail.com>
2019-07-13 22:50:21 -0500

    Add attack animations

--- core/src/me/jakerg/gdxtest/creature/Player.java
+++ core/src/me/jakerg/gdxtest/creature/Player.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Stack;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.Texture;
@@ -21,12 +22,16 @@ import me.jakerg.gdxtest.screens.DungeonScreen;
 import me.jakerg.rougelike.Move;
 
 public class Player extends Sprite implements InputProcessor{
+	enum State { WALK, ATTACK };
+	
 	
 	private final int SPEED = 200;
 	private Stack<Move> inputStack;
 	private Move lastMove;
+	private State state;
 	public Animation<TextureRegion> runningAnimation;
 	public HashMap<Move, Animation<TextureRegion>> animations;
+	public HashMap<Move, Animation<TextureRegion>> attackRegions;
 	private float elapsedTime;
 	
 	public Player(DungeonScreen screen) {
@@ -38,6 +43,7 @@ public class Player extends Sprite implements InputProcessor{
 		inputStack = new Stack<>();
 		inputStack.push(Move.NONE);
 		lastMove = Move.UP;
+		state = State.WALK;
 		
 		setUpAnimations(screen);
 		
@@ -72,6 +78,29 @@ public class Player extends Sprite implements InputProcessor{
 		a = new Animation<>(s, regions, PlayMode.LOOP);
 		animations.put(Move.RIGHT, a);
 		regions.clear();
+		
+		
+		s = 1/5f;
+		attackRegions = new HashMap<>();
+		regions.add(screen.atlas.findRegion("attackup"));
+		a = new Animation<>(s, regions, PlayMode.NORMAL);
+		attackRegions.put(Move.UP, a);
+		regions.clear();
+		
+		regions.add(screen.atlas.findRegion("attackdown"));
+		a = new Animation<>(s, regions, PlayMode.NORMAL);
+		attackRegions.put(Move.DOWN, a);
+		regions.clear();
+		
+		regions.add(screen.atlas.findRegion("attackleft"));
+		a = new Animation<>(s, regions, PlayMode.NORMAL);
+		attackRegions.put(Move.LEFT, a);
+		regions.clear();
+		
+		regions.add(screen.atlas.findRegion("attackright"));
+		a = new Animation<>(s, regions, PlayMode.NORMAL);
+		attackRegions.put(Move.RIGHT, a);
+		regions.clear();
 	}
 
 	public void update(float delta) {
@@ -79,12 +108,23 @@ public class Player extends Sprite implements InputProcessor{
 		if(move == null)
 			move = Move.NONE;
 		
+		if(state.equals(State.ATTACK)) move = Move.NONE;
+		
 		if(!move.equals(Move.NONE)) {
 			lastMove = move;
 			elapsedTime += delta;
 		}
+		
+		if(state.equals(State.WALK))
+			this.setRegion(animations.get(lastMove).getKeyFrame(elapsedTime, true));
+		else if(state.equals(State.ATTACK)) {
+			elapsedTime += delta;
+			this.setRegion(attackRegions.get(lastMove).getKeyFrame(elapsedTime, true));
 
-		this.setRegion(animations.get(lastMove).getKeyFrame(elapsedTime, true));
+			if(attackRegions.get(lastMove).isAnimationFinished(elapsedTime)) {
+				state = State.WALK;
+			}
+		}
 		this.getTexture().setFilter(TextureFilter.Nearest, TextureFilter.Nearest);
 //		this.setScale(1, 1);
 		float dX = move.x() * SPEED * delta;
@@ -110,7 +150,14 @@ public class Player extends Sprite implements InputProcessor{
 	public boolean keyTyped(char character) { return false; }
 
 	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) { return false; }
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) { 
+		if(button == Buttons.LEFT) {
+			state = State.ATTACK;
+			elapsedTime = 0;
+			return true;
+		}
+		return false; 
+	}
 
 	@Override
 	public boolean touchUp(int screenX, int screenY, int pointer, int button) { return false; }
